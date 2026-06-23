bdc61cfde0d9d08721ff5c332cc6622001468f50
tobiusaolo <tobiusaolo21@gmail.com>
2026-06-23 09:37:21 +0300

    changes

--- src/utils/deliveryTime.js
+++ src/utils/deliveryTime.js
@@ -4,7 +4,7 @@ export const DELIVERY_TIME_OPTIONS = [
   { value: 'ONE_WEEK', label: '1 week' },
   { value: 'TWO_WEEKS', label: '2 weeks' },
   { value: 'THREE_WEEKS', label: '3 weeks' },
-  { value: 'ONE_MONTH', label: 'A month' },
+  { value: 'ONE_MONTH', label: '1 month' },
 ];
 
 export const DEFAULT_DELIVERY_TIME = 'THREE_DAYS';
